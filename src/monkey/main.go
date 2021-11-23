ccc04b453aa7ad135e666661d1dc420107d84916
smoh <wishforkiss@naver.com>
2021-11-23 22:52:10 +0900

    first commit

---
+++ src/monkey/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"os/user"
+	//"github.com/kitasuke/monkey-go/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		print(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Monky programming language!\n", user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	//repl.Start(os.Stdin, os.Stdout)
+}
