d3d21369051e88747fc49c51c902fb24dbeefe68
unknown <wishforkiss@naver.com>
2021-12-12 14:04:26 +0900

    evaluator, object 코드 파일

---
+++ src/monkey/evaluator/evaluator.go
@@ -0,0 +1,79 @@
+package evaluator
+
+import (
+	"fmt"
+	"monkey/ast"
+	"monkey/object"
+)
+
+var (
+	NULL  = &object.Null{}
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+)
+
+func Eval(node ast.Node) object.Object {
+	fmt.Println(node.String())
+
+	switch node := node.(type) {
+	case *ast.Program: // program statement root를 만난 경우
+		return evalStatements(node.Statements)
+
+	case *ast.ExpressionStatement: // expression 표현식을 만난 경우
+		return Eval(node.Expression)
+
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
+
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
+	}
+
+	return nil
+}
+
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+
+	// statement 단위로 평가를 한다.
+	for _, statement := range stmts {
+		result = Eval(statement)
+	}
+
+	return result
+}
+
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+
+	return FALSE
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	default:
+		return NULL
+	}
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
+}
